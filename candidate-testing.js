d0e1da7e5d07a0272bb0c3dcb5e25a5aa238983b
ohCrud <ohcrud@crudbook.lan>
2024-01-22 14:19:54 -0600

    Completed Part 2
    Passing npm test 1 through 10

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,7 +1,10 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+// Current state: Part 2 finished, npm test passes tests 1 through 10.
 
+
+
+// TODO 2: modify your quiz app to ask 5 questions //
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -11,10 +14,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,18 +25,45 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]));
+  }
 }
+
+// let arr2 = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+// let evens = [], odds = [];
+
+// for (let i = 0; i < 10; i++) {
+//   if (arr2[i] % 2 === 0) {
+//     evens.push(arr2[i]);
+//   } else {
+//     odds.push(arr2[i]);
+//   }
+// }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) { 
-  console.log("Correct")
-} else {
-  console.log("Incorrect")
+  // if (candidateAnswer == correctAnswer) { 
+  //   console.log("Correct")
+  // } else {
+  //   console.log("Incorrect")
+  // }
+  console.log("\nTest Results")
+  for (let i = 0; i < correctAnswers.length; i++) {
+    let result;
+    if (candidateAnswers[i] == correctAnswers[i]) {
+      result = "Correct!";
+    } else {
+      result = "Incorrect."
+    }
 
-}
+    console.log(`
+    Question 1: ${questions[i]}
+    Your Answer: ${candidateAnswers[i]}
+    Correct Answer: ${correctAnswers[i]}
+    Result: ${result}`)
+  }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
